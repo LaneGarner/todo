ef6b3a8492b10c9bbc840e13291ea3bad5d0ee52
Lane Garner <b.lane.garner@gmail.com>
2021-05-31 11:43:32 -0500

    added redu scaffolding

---
+++ src/store/action/user.js
@@ -0,0 +1,15 @@
+// export const GET_USERS = 'GET_USERS';
+
+// export const getUsers = () => {
+//   return async dispatch => {
+//     try {
+//       const userData = await AsyncStorage.getItem('users');
+//       dispatch({
+//         type: GET_USERS,
+//         payload: userData,
+//       });
+//     } catch (err) {
+//       throw err;
+//     }
+//   };
+// };
---
+++ src/store/index.js
@@ -0,0 +1,6 @@
+// import {createStore, applyMiddleware} from 'redux';
+// import thunk from 'redux-thunk';
+
+// import {users} from './reducer/user';
+
+// export default createStore(users, applyMiddleware(thunk));
